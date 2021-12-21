8957bd7254c5d8f2e0b151e006b5153b80f62f4c
KennaReed <mreed16@calpoly.edu>
2021-12-20 16:49:41 -0800

    set up backend

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+
+function helloHandler(request, response) {
+    console.log("Request Made")
+	const name = request.query.name
+
+	response.status(200)
+	response.send('Hello, ' + name + '!')
+}
+
+app.get('/hello', helloHandler)
+
+app.listen(3000, () => console.log('server running on port 3000'))
