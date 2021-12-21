c7b75add87c42ea7c91bdf300f439f870d247fab
KennaReed <mreed16@calpoly.edu>
2021-12-20 17:58:57 -0800

    set up Prettier

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 function helloHandler(request, response) {
-    console.log("Request Made")
-	const name = request.query.name
+  console.log("Request Made");
+  const name = request.query.name;
 
-	response.status(200)
-	response.send('Hello, ' + name + '!')
+  response.status(200);
+  response.send("Hello, " + name + "!");
 }
 
-app.get('/hello', helloHandler)
+app.get("/hello", helloHandler);
 
-app.listen(3000, () => console.log('server running on port 3000'))
+app.listen(3000, () => console.log("server running on port 3000"));
