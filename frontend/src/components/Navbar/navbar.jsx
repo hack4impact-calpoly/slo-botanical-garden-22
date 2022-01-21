00f73abf0402b139b98e5d99c235c7c332d98418
Carter Chan-Nui <carter@vulkan.local>
2022-01-21 14:18:13 -0800

    Updated navBar

--- frontend/src/components/Navbar/navbar.jsx
+++ frontend/src/components/Navbar/navbar.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import './navbar.css'
+import logo from '../../assets/logo.png'
+import vector from '../../assets/vector.png'
+import userIcon from '../../assets/user-icon.png'
 import {Link} from 'react-router-dom';
 
 export default function Navbar() {
   return (
     <nav>
-      <Link to="/"><img src="../../assets/logo.png" alt='logo'/></Link>
-      <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">Profile</Link></li>
-      </ul>
+      <div class="navFlex">
+          <Link to="/"><img src={logo} alt='logo'/></Link>
+          <img id="align-nav-left" src={vector} />
+          <h1 id="align-nav-left">Volunteer Database</h1>
+      </div>
+
+      <div class="navFlex">
+        <h1 id="align-nav-right"><Link to="/">Home</Link></h1>
+        <h1 id="align-nav-right"><Link to="/about">Profile</Link></h1>
+        <img id="user-icon" src={userIcon}/>
+      </div>
     </nav>
   );
 }
