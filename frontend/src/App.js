46ffd22bf9f8cdf1be7be473c856936016f37bd6
Arden <ardenozdere@gmail.com>
2022-02-28 16:23:09 -0800

    login basically done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import "./App.css";
 import Navbar from "./components/Navbar/navbar";
+import Login from "./components/Login/login";
 import Footer from "./components/Footer/footer";
 import Home from "./components/Home/home";
+import awsconfig from './aws-exports'; 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
+    <Login />
+    // <Router>
+    //   <Navbar />
 
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
-      </Routes>
-      <Footer />
-    </Router>
+    //   <Routes>
+    //     <Route exact path="/" element={<Home />} />
+    //     {/* <Route path="/about" element={<About />} /> */}
+    //   </Routes>
+    //   <Footer />
+    // </Router>
   );
 }
 
