cf2a73526f92a20267262682a939f0906f4ab379
Carter Chan-Nui <26908609+carterchannui@users.noreply.github.com>
2022-03-07 19:37:24 -0800

    refactored navbar with ChakraUI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,21 @@ import Navbar from "./components/Navbar/navbar";
 import Footer from "./components/Footer/footer";
 import Home from "./components/Home/home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
+      <ChakraProvider>
+        <Router>
+          <Navbar />
 
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
-      </Routes>
-      <Footer />
-    </Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} /> */}
+          </Routes>
+          <Footer />
+        </Router>
+      </ChakraProvider>
   );
 }
 
