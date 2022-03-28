a9ebde39383791e7a44794733b45e6b1bb7b6abf
Arden <32276300+arden2000@users.noreply.github.com>
2022-03-28 10:59:07 -0700

    User auth - login page (#40)
    * login basically done
    
    * sign up disappears when forgot password
    
    * more styling
    
    * removed login from app.js
    
    * appjs
    
    * adding OR
    
    * all styling done, adding cognito connection
    
    * Added dynamo table functions
    
    * changed App to see whole site, not just login
    
    Co-authored-by: Arden Ozdere <arden@Ardens-MacBook-Pro.local>
    Co-authored-by: KennaReed <mreed16@calpoly.edu>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/navbar";
+import Login from "./components/Login/login";
 import Footer from "./components/Footer/footer";
 import Home from "./components/Home/home";
+import awsconfig from "./aws-exports";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    //<Login></Login>
     <Router>
       <Navbar />
 
