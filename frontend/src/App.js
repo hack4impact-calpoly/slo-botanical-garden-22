ca5e0403cda842554832b6198c0858ea912e3988
KennaReed <mreed16@calpoly.edu>
2022-03-28 10:57:42 -0700

    changed App to see whole site, not just login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,21 @@ import Navbar from "./components/Navbar/navbar";
 import Login from "./components/Login/login";
 import Footer from "./components/Footer/footer";
 import Home from "./components/Home/home";
-import awsconfig from './aws-exports'; 
+import awsconfig from "./aws-exports";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Login></Login>
-    // <Router>
-    //   <Navbar />
+    //<Login></Login>
+    <Router>
+      <Navbar />
 
-    //   <Routes>
-    //     <Route exact path="/" element={<Home />} />
-    //     {/* <Route path="/about" element={<About />} /> */}
-    //   </Routes>
-    //   <Footer />
-    // </Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
