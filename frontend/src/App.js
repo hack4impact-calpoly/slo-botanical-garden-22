900c31be6617afffe8ff8f888dec12a8d0b9ed2a
Carter Chan-Nui <26908609+carterchannui@users.noreply.github.com>
2022-03-28 11:36:14 -0700

    Refactored home dashboard with ChakraUI (#41)
    * login basically done
    
    * sign up disappears when forgot password
    
    * more styling
    
    * removed login from app.js
    
    * appjs
    
    * refactored navbar with ChakraUI
    
    * refactored with ChakraUI
    
    * adding OR
    
    * all styling done, adding cognito connection
    
    * Added dynamo table functions
    
    * changed App to see whole site, not just login
    
    Co-authored-by: Arden <ardenozdere@gmail.com>
    Co-authored-by: Arden Ozdere <arden@Ardens-MacBook-Pro.local>
    Co-authored-by: KennaReed <mreed16@calpoly.edu>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,19 +5,21 @@ import Footer from "./components/Footer/footer";
 import Home from "./components/Home/home";
 import awsconfig from "./aws-exports";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
-    //<Login></Login>
-    <Router>
-      <Navbar />
+      //<Login></Login>
+      <ChakraProvider>
+        <Router>
+          <Navbar />
 
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
-      </Routes>
-      <Footer />
-    </Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            {/* <Route path="/about" element={<About />} /> */}
+          </Routes>
+        </Router>
+      </ChakraProvider>
   );
 }
 
