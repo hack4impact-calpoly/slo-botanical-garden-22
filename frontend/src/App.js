e277a6af25a03f27333a805607a2eb6589d1155a
Arden Ozdere <ardenozdere@gmail.com>
2022-03-29 19:44:35 -0700

    login changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,26 @@ import Navbar from "./components/Navbar/navbar";
 import Login from "./components/Login/login";
 import Footer from "./components/Footer/footer";
 import Home from "./components/Home/home";
-import awsconfig from './aws-exports'; 
+import SignUpForm from "./components/SignUpForm";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect, useLocation } from 'react';
 
-function App() {
+function App(props) {
+  var [isNavbarHidden, setIsNavbarHidden] = useState(false);
+  useEffect(() => {
+      setIsNavbarHidden(false);
+  }, []);
   return (
-    <Login></Login>
-    // <Router>
-    //   <Navbar />
-
-    //   <Routes>
-    //     <Route exact path="/" element={<Home />} />
-    //     {/* <Route path="/about" element={<About />} /> */}
-    //   </Routes>
-    //   <Footer />
-    // </Router>
+    <Router>
+      {isNavbarHidden ? null : <Navbar />}
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        {/* <Route path="/about" element={<About />} /> */}
+        <Route path="signup" element={<SignUpForm />} />
+        <Route path='/login'  element={<Login setNavbar={setIsNavbarHidden} />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
--- frontend/src/components/Login/login.jsx
+++ frontend/src/components/Login/login.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
-import { Authenticator, useTheme, Heading, useAuthenticator, View, Button } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import './login.css';
+import React, { useState, useEffect }  from "react";
+import {
+  Authenticator,
+  useTheme,
+  Heading,
+  useAuthenticator,
+  View,
+  Button,
+} from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import "./login.css";
+import { Link } from "react-router-dom";
+import SignUpForm from "../SignUpForm";
 
 const components = {
   SignIn: {
@@ -10,37 +19,54 @@ const components = {
       return (
         <Heading
           padding={`${tokens.space.xl} 0 0 ${tokens.space.xl}`}
-          level={3} className="signInHeading">
-        PLEASE SIGN IN
+          level={3}
+          className="signInHeading"
+        >
+          PLEASE SIGN IN
         </Heading>
       );
     },
-  }, 
+  },
   Footer() {
-    const { route } = useAuthenticator(context => [context.route]);
-    return route === 'resetPassword' ? null :
-    (
+    const { route } = useAuthenticator((context) => [context.route]);
+    return route === "resetPassword" ? null : (
       <View textAlign="center">
-        <h2 style={{marginTop: '.5em'}} className="OR"><span>OR</span></h2>
-        <Button
-          fontWeight="normal"
-          onClick={() => alert('route to sign up')}
-          variation="primary"
-          size="Large"
-          isFullWidth={true}
-          style={{width: '45%', marginTop: '1em', background: "#aeb5ad", color: "#576754", fontFamily: "SBG"}}
-        >
-          Sign Up
-        </Button>
+        <h2 style={{ marginTop: ".5em" }} className="OR">
+          <span>OR</span>
+        </h2>
+        <Link to="/signup">
+          <Button
+            fontWeight="normal"
+            variation="primary"
+            size="Large"
+            isFullWidth={true}
+            style={{
+              width: "45%",
+              marginTop: "1em",
+              background: "#aeb5ad",
+              color: "#576754",
+              fontFamily: "SBG",
+            }}
+          >
+            Sign Up
+          </Button>
+        </Link>
       </View>
     );
   },
 };
 
-export default function Login() {
+export default function Login({ setNavbar }) {
+  useEffect(() => {
+    setNavbar(true);
+  }, []);
   return (
     <div className="signInPage">
-      <Authenticator className="signIn" hideSignUp={true} components={components}>
+      <Authenticator
+        className="signIn"
+        hideSignUp={true}
+        components={components}
+      >
         {({ signOut, user }) => (
           <main>
             <h1>Hello {user.username}</h1>
