e11699d246f8f3ffb283b7aadbd049bf72722dfc
Noah Shah <noshah@calpoly.edu>
2022-04-04 14:55:39 -0700

    Added sorting to react table

--- frontend/src/components/VolunteerTable/volunteerTable.js
+++ frontend/src/components/VolunteerTable/volunteerTable.js
@@ -1,20 +1,25 @@
 import React, { useState } from "react";
-import { useTable } from "react-table";
+import { useSortBy, useTable } from "react-table";
 import "./volunteerTable.css";
+import bgimage from "../../assets/garden.png";
+import { Flex, Table, Box } from "@chakra-ui/react";
 
 const VolunteerTable = () => {
   const data = React.useMemo(
     () => [
       {
-        name: "some name",
+        firstName: "some",
+        lastName: "pon",
         contact: "name@mail.com",
       },
       {
-        name: "other name",
+        firstName: "other",
+        lastName: "ffon",
         contact: "other@mail.com",
       },
       {
-        name: "pi",
+        firstName: "pi",
+        lastName: "aa",
         contact: "sss@mail.com",
       },
     ],
@@ -25,14 +30,14 @@ const VolunteerTable = () => {
     () => [
       {
         Header: "Name",
-        accessor: "name", // accessor is the "key" in the data
-        getProps: () => {
-          return {
-            headerStyle: {
-              color: "red",
-            },
-          };
-        },
+        accessor: "firstName", // accessor is the "key" in the data
+        Cell: row => {
+          return (
+            <div>
+              <span>{row.row.original.firstName + " " + row.row.original.lastName}</span>
+            </div>
+          )
+        }
       },
       {
         Header: "Contact",
@@ -43,53 +48,72 @@ const VolunteerTable = () => {
   );
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data });
+    useTable({ columns, data, initialState: {
+      sortBy: [
+        {
+          id: "firstName",
+        }
+      ]
+    } }, useSortBy);
 
   return (
-    <table
-      {...getTableProps()}
-      style={{ boxShadow: "0 0 20px rgba(0,0,0,0.8)", background: "#CCDDBD" }}
+    <div className="container">
+    <Flex
+      p={10}
+      w="100%"
+      bgImage={bgimage}
+      bgPosition="center"
+      bgSize="cover"
+      bgRepeat="no-repeat"
     >
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th
-                {...column.getHeaderProps()}
-                style={{
-                  borderRight: "none",
-                  background: "#576754",
-                  color: "white",
-                }}
-              >
-                {column.render("Header")}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <td
-                    {...cell.getCellProps()}
+      <Box> 
+        <table
+          {...getTableProps()}
+          style={{ boxShadow: "0 0 20px rgba(0,0,0,0.8)", background: "#CCDDBD", borderSpacing: "0 1em" }}
+        >
+          <thead>
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th
+                    {...column.getHeaderProps()}
                     style={{
-                      background: "#e6f2d9",
+                      borderRight: "none",
+                      background: "#576754",
+                      color: "white",
                     }}
                   >
-                    {cell.render("Cell")}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => {
+                    return (
+                      <td
+                        {...cell.getCellProps()}
+                        style={{
+                          background: "#e6f2d9",
+                        }}
+                      >
+                        {cell.render("Cell")}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </Box>
+    </Flex>
+    </div>
   );
 };
 
